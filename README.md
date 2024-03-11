6edbf98312eadb538e118745c9e33840fc6ca8ab
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2024-03-10 20:45:30 -0500

    Improve UI labels and add examples/instructions in README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# thing-not-thing
+# Thing/Not Thing
 Zero-shot binary image classifier. ELI5: Type in an object and it predicts whether a photo is that object.
+
+## Examples
+
+![Hot Dog Detected](/readme-images/HotDog.png?raw=true)
+
+![Hot Dog Not Detected](/readme-images/NotHotDog.png?raw=true)
+
+## Try It Yourself!
+
+This project is available as a web demo [here](https://huggingface.co/spaces/jbinvnt/thing-not-thing). But it will be slower than when the project is run locally on a GPU, as explained below.
+
+## Running Locally on a GPU
+
+Tested on [Debian](https://debian.org).
+
+### Requirements
+
+- [NVIDIA Container Toolkit](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html)
+- [Docker](https://docs.docker.com/engine/install/) (and Docker Compose)
+- An NVIDIA GPU with sufficient VRAM for your chosen ViT model (which can be changed in [app.py](app.py))
+
+### Startup
+
+```bash
+docker compose build
+docker compose run torch
+```
+
+Then go to `https://localhost:7860` in your browser.
--- app.py
+++ app.py
@@ -4,17 +4,23 @@ import open_clip
 import gradio as gr
 import os
 
-local_gpu = os.environ["RUN_MODE"] == "local_gpu" if "RUN_MODE" in os.environ else False
+use_gpu = False
+load_local_model = False
+
+if "RUN_MODE" in os.environ:
+    run_mode = os.environ["RUN_MODE"]
+    use_gpu = "gpu" in run_mode
+    load_local_model = "local" in run_mode
 
 def is_thing(image, name_of_thing, threshold, output_similarity):
-    model_path = '/clip/CLIP-ViT-L-14-laion2B-s32B-b82K/open_clip_pytorch_model.bin' if local_gpu else 'laion2B_s32B_b82K'
-    model, _, preprocess = open_clip.create_model_and_transforms('ViT-L-14', pretrained=model_path)
-    tokenizer = open_clip.get_tokenizer('ViT-L-14')
+    model_path = "/clip/CLIP-ViT-L-14-laion2B-s32B-b82K/open_clip_pytorch_model.bin" if load_local_model else "laion2B_s32B_b82K"
+    model, _, preprocess = open_clip.create_model_and_transforms("ViT-L-14", pretrained=model_path)
+    tokenizer = open_clip.get_tokenizer("ViT-L-14")
 
     image = preprocess(image).unsqueeze(0)
     text = tokenizer([name_of_thing, ])
 
-    with torch.no_grad(), torch.cuda.amp.autocast() if local_gpu else torch.cpu.amp.autocast():
+    with torch.no_grad(), torch.cuda.amp.autocast() if use_gpu else torch.cpu.amp.autocast():
         image_features = model.encode_image(image)
         text_features = model.encode_text(text)
         image_features /= image_features.norm(dim=-1, keepdim=True)
@@ -24,5 +30,5 @@ def is_thing(image, name_of_thing, threshold, output_similarity):
     similarity = similarity_output.data.tolist()[0][0]
     return ("Not " if similarity < threshold else "") + name_of_thing + "." + (" Similarity = {}.".format(similarity) if output_similarity else "")
 
-demo = gr.Interface(fn=is_thing, inputs=[gr.Image(type="pil"), "text"], additional_inputs=[gr.Slider(0, 1, value=0.25), "checkbox"], outputs=["text", ])
+demo = gr.Interface(fn=is_thing, inputs=[gr.Image(type="pil"), gr.Textbox(label="Is this a...")], additional_inputs=[gr.Slider(0, 1, value=0.25), gr.Checkbox(label="Output the similarity score")], outputs=["text", ])
 demo.launch()
