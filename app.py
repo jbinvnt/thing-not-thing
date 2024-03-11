4351ca43873196bace35d10c1c74ac9f0bbd2472
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2024-03-10 19:00:15 -0500

    Switch to outputting just the cosine similarity of features

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from PIL import Image
 import open_clip
 import gradio as gr
 
-def is_thing(image, name_of_thing, other_class, threshold): # With the dfeault example, softmax is computed over classes so w/ just 1 class the output is always 1.00
+def is_thing(image, name_of_thing, threshold):
     model, _, preprocess = open_clip.create_model_and_transforms('ViT-L-14', pretrained='/clip/CLIP-ViT-L-14-laion2B-s32B-b82K/open_clip_pytorch_model.bin')
     tokenizer = open_clip.get_tokenizer('ViT-L-14')
 
     image = preprocess(image).unsqueeze(0)
-    text = tokenizer([name_of_thing, other_class])
+    text = tokenizer([name_of_thing, ])
 
     with torch.no_grad(), torch.cuda.amp.autocast():
         image_features = model.encode_image(image)
@@ -16,9 +16,9 @@ def is_thing(image, name_of_thing, other_class, threshold): # With the dfeault e
         image_features /= image_features.norm(dim=-1, keepdim=True)
         text_features /= text_features.norm(dim=-1, keepdim=True)
 
-        text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-    
-    return ("Not " if text_probs.data.tolist()[0][0] < threshold else "") + name_of_thing + " (probability = {})".format(text_probs.data.tolist()[0][0])
+        similarity_output = image_features @ text_features.T
+    similarity = similarity_output.data.tolist()[0][0]
+    return ("Not " if similarity < threshold else "") + name_of_thing + " (similarity = {})".format(similarity)
 
-demo = gr.Interface(fn=is_thing, inputs=[gr.Image(type="pil"), "text", "text", gr.Slider(0, 1, value=0.25)], outputs=["text", ])
+demo = gr.Interface(fn=is_thing, inputs=[gr.Image(type="pil"), "text", gr.Slider(0, 1, value=0.25)], outputs=["text", ])
 demo.launch()
