3b1efc190b437062206179f6ebe021dca042047a
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2024-03-10 19:11:03 -0500

    Hide the threshold and similarity by default

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import open_clip
 import gradio as gr
 
-def is_thing(image, name_of_thing, threshold):
+def is_thing(image, name_of_thing, threshold, output_similarity):
     model, _, preprocess = open_clip.create_model_and_transforms('ViT-L-14', pretrained='/clip/CLIP-ViT-L-14-laion2B-s32B-b82K/open_clip_pytorch_model.bin')
     tokenizer = open_clip.get_tokenizer('ViT-L-14')
 
@@ -18,7 +18,7 @@ def is_thing(image, name_of_thing, threshold):
 
         similarity_output = image_features @ text_features.T
     similarity = similarity_output.data.tolist()[0][0]
-    return ("Not " if similarity < threshold else "") + name_of_thing + " (similarity = {})".format(similarity)
+    return ("Not " if similarity < threshold else "") + name_of_thing + "." + (" Similarity = {}.".format(similarity) if output_similarity else "")
 
-demo = gr.Interface(fn=is_thing, inputs=[gr.Image(type="pil"), "text", gr.Slider(0, 1, value=0.25)], outputs=["text", ])
+demo = gr.Interface(fn=is_thing, inputs=[gr.Image(type="pil"), "text"], additional_inputs=[gr.Slider(0, 1, value=0.25), "checkbox"], outputs=["text", ])
 demo.launch()
